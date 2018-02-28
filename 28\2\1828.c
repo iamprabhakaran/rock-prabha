afbb86d0329e4630b7801349ff4321e331edacb0
iamprabhakaran <35688582+iamprabhakaran@users.noreply.github.com>
2018-02-28 15:53:26 +0530

    Create 28\2\1828.c

---
+++ 28\2\1828.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a[250],i,,n;
+printf("Enter the  number");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+printf("enter the value");
+  scanf("%d",&a[i]);
+}
+  for(i=0;i<=n;i++)
+  {
+
+  
+  printf("maximum element is %d%d",a[i],i);
+  getch();
+}
+}
