a25909e2cbc92fb74ea8a18e4bfcc1fe99af87a0
iamprabhakaran <35688582+iamprabhakaran@users.noreply.github.com>
2018-02-28 15:56:54 +0530

    Create 28\2\1829.c

---
+++ 28\2\1829.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+long min,hr,time;
+printf("Enter the time");
+scanf("%ld",&time);
+hr=time/60;
+min=time%60;
+printf("The time is converted into min and hour:%ld%ld",min,hr);
+getch();
+}
